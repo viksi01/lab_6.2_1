387feb2dbb756013e5b31252a179487dd4c6ad61
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-11 09:02:02 +0200

    master-commit1

---
+++ UnitTest_6.2.1/UnitTest_6.2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.2_1/lab_6.2_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest621
+{
+	TEST_CLASS(UnitTest621)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int r[5] = { -34,64,61,-40,-8 };
+			int t = Max(r, 5);
+			Assert::AreEqual(t, 61);
+		}
+	};
+}
---
+++ lab_6.2_1/lab_6.2_1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void Show(int* a, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = - 100 + rand() % (-100 - 100) + 1;
+	}
+}
+
+void Print(int* a, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ",";
+	}
+}
+
+
+int Max(int* a, const int n)
+{
+	int max;
+	int imax = -1;
+	for (int i = 0; i < n; i++)
+		if (a[i] % 2 != 0)
+		{
+			max = a[i];
+			imax = i;
+			break;
+		}
+	for (int i = imax + 1; i < n; i++)
+		if (a[i] > max && a[i] % 2 != 0)
+			max = a[i];
+	return max;
+	
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 20;
+	int a[n];
+	Show(a, n);
+	cout << "a {"; Print(a, n); cout << "}" << endl;
+	cout << "Max = " << Max(a, n) << endl;
+}
